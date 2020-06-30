c1f4b679bcfa2564ff470330cb987753b02609f7
empjenk <empjenk@ericsson.com>
2020-06-30 15:45:37 +0000

    r3.0.5-BETA-3

--- src/main/java/com/redbeemedia/enigma/core/http/MockHttpHandler.java
+++ src/main/java/com/redbeemedia/enigma/core/http/MockHttpHandler.java
@@ -10,23 +10,27 @@ import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.regex.Pattern;
 
 public class MockHttpHandler implements IHttpHandler {
     private Queue<IHttpHandler> responses = new LinkedList<>();
+    private Map<Pattern, Queue<IHttpHandler>> specialResponses = new HashMap<>();
     private List<String> log = new ArrayList<>();
 
     @Override
     public void doHttp(URL url, IHttpCall httpCall, IHttpResponseHandler response) {
         try {
+            String urlString = url.toString();
             MockHttpConnection mockHttpConnection = new MockHttpConnection();
             httpCall.prepare(mockHttpConnection);
             JSONObject logEntry = new JSONObject();
             logEntry.put("method", httpCall.getRequestMethod());
-            logEntry.put("url", url.toString());
+            logEntry.put("url", urlString);
             JSONObject headerMap = new JSONObject();
             for (Map.Entry<String, String> header : mockHttpConnection.getHeaders().entrySet()) {
                 headerMap.put(header.getKey(), header.getValue());
@@ -36,12 +40,28 @@ public class MockHttpHandler implements IHttpHandler {
             httpCall.writeBodyTo(byteArrayOutputStream);
             logEntry.put("body", new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8));
             log.add(logEntry.toString());
-            if(!responses.isEmpty()) {
-                responses.poll().doHttp(url, httpCall, response);
+            IHttpHandler responseHandler = getReponseHandler(urlString);
+            if(responseHandler != null) {
+                responseHandler.doHttp(url, httpCall, response);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private IHttpHandler getReponseHandler(String urlString) {
+        for(Pattern pattern : specialResponses.keySet()) {
+            if(pattern.matcher(urlString).matches()) {
+                Queue<IHttpHandler> specialQueue = specialResponses.get(pattern);
+                if(!specialQueue.isEmpty()) {
+                    return specialQueue.poll();
+                }
+            }
+        }
+        if(!responses.isEmpty()) {
+            return responses.poll();
+        }
+        return null;
     }
 
     @Override
@@ -67,5 +87,14 @@ public class MockHttpHandler implements IHttpHandler {
 
     public List<String> getLog() {
         return log;
+    }
+
+    public void queueResponse(Pattern urlPattern, HttpStatus httpStatus, String responseBody) {
+        Queue<IHttpHandler> queue = specialResponses.get(urlPattern);
+        if(queue == null) {
+            queue = new LinkedList<>();
+            specialResponses.put(urlPattern, queue);
+        }
+        queue.add(new MockOnResponseResponse(httpStatus, responseBody.getBytes(StandardCharsets.UTF_8)));
     }
 }
--- src/main/java/com/redbeemedia/enigma/core/playrequest/MockPlaybackProperties.java
+++ src/main/java/com/redbeemedia/enigma/core/playrequest/MockPlaybackProperties.java
@@ -1,8 +1,15 @@
 package com.redbeemedia.enigma.core.playrequest;
 
+import com.redbeemedia.enigma.core.format.IMediaFormatPreferenceSpec;
+
 public class MockPlaybackProperties implements IPlaybackProperties {
     @Override
     public PlayFrom getPlayFrom() {
         return PlayFrom.PLAYER_DEFAULT;
+    }
+
+    @Override
+    public IMediaFormatPreferenceSpec getMediaFormatPreferences() {
+        return null;
     }
 }
